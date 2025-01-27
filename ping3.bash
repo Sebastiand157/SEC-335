836d1839cd2e585ad989a06570909894755191fd
Sebastiand157 <113308434+Sebastiand157@users.noreply.github.com>
2025-01-27 15:07:25 -0500

    Delete ping3.bash

--- ping3.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-output_file="sweep3.txt"
-> "$output_file"
-nmap -sn 10.0.5.2-50 -oG - | awk '/Up$/{print $2}' > "$output_file"
-echo "Sweep results saved in $output_file:"
-cat "$output_file"
